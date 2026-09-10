99a2a3643040f1546474743f03f374fb2338beee
Jared <jgeilich@gmail.com>
2026-09-10 13:23:56 -0400

    add back autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     tsconfigPaths: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
   },
   server: {
     proxy: {
